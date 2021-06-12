5caaae3ecd4213e1b81aba122e9173acc6fc7c09
reidjs <reidsherman@gmail.com>
2021-06-12 16:37:02 -0700

    test arrays and slices

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,18 +1,21 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	t.Run("Collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
+	// t.Run("Collection of 5 numbers", func(t *testing.T) {
+	// 	numbers := []int{1, 2, 3, 4, 5}
 
-		got := Sum(numbers)
-		want := 15
+	// 	got := Sum(numbers)
+	// 	want := 15
 
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
-	})
+	// 	if got != want {
+	// 		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	// 	}
+	// })
 
 	t.Run("Collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
@@ -24,4 +27,41 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	numbers1 := []int{1, 2, 3}
+	numbers2 := []int{0, 9}
+	numbers3 := []int{0, 9}
+
+	got := SumAll(numbers1, numbers2, numbers3)
+	want := []int{6, 9, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("%v and %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of slices ignoring first digit", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{1, 2, 3})
+		want := []int{0, 5}
+
+		checkSums(t, got, want)
+	})
+
 }
